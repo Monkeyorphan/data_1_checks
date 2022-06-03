bd0d0c72df11eb4e139774861717148be1380ab1
Troy Nally <105830690+Monkeyorphan@users.noreply.github.com>
2022-06-02 21:39:01 -0400

    Add files via upload

---
+++ data1checks.py
@@ -0,0 +1,10 @@
+print('hello world!')
+
+louisville_distilleries = ['Michters', 'Old Forester', 'Rabbit Hole', 'Angels Envy', 'Evan Williams', 'Stitzel-Weller', 'Peerless']
+print(louisville_distilleries[0])
+
+bourbon_brands = {'Eagle Rare':'Buffalo Trace', 'Larceny':'Heaven Hill', 'Blood Oath':'Lux Row', 'Bookers':'James B Beam'}
+print(bourbon_brands['Larceny'])
+    
+allocated_bourbon = ('Weller', 'E.H. Taylor', "Blanton's", 'Eagle Rare')
+print(allocated_bourbon[2])
